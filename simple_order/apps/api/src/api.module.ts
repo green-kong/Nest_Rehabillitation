4d350e7bfa7413b3cda46e452cf72346e1989b75
dev_kong <ahdjd5@gmail.com>
2023-12-07 23:00:40 +0900

    chore: 사용하지 않는 파일 제거

--- simple_order/apps/api/src/api.module.ts
+++ simple_order/apps/api/src/api.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ApiController } from './api.controller';
-import { ApiService } from './api.service';
 import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from '../../../libs/config/TypeORM.config';
 
 @Module({
     imports: [ProductModule, TypeOrmModule.forRoot(typeORMConfig)],
-    controllers: [ApiController],
-    providers: [ApiService],
 })
 export class ApiModule {}
