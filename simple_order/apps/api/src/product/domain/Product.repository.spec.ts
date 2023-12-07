b1b0e2df249ce47ce4cef0d5cd05af633c97a33d
dev_kong <ahdjd5@gmail.com>
2023-12-07 22:59:01 +0900

    test: product findById 테스트 추가

--- simple_order/apps/api/src/product/domain/Product.repository.spec.ts
+++ simple_order/apps/api/src/product/domain/Product.repository.spec.ts
@@ -41,5 +41,21 @@ describe('프로덕트 리포지토리 테스트', () => {
             expect(savedProduct.getName()).toEqual(name);
             expect(savedProduct.getPrice()).toEqual(price);
         });
+
+        it('저장한 product를 조회한다.', async () => {
+            // given
+            const price = 3500;
+            const name = '순대';
+            const product = Product.of(name, price);
+            const savedProduct = await repository.save(product);
+
+            // when
+            const foundProduct = await repository.findById(savedProduct.id);
+
+            // then
+            expect(foundProduct.id).toEqual(savedProduct.id);
+            expect(foundProduct.getName()).toEqual(name);
+            expect(foundProduct.getPrice()).toEqual(price);
+        });
     });
 });
